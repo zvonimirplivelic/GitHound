1c3d83e00e764edfa0107b2ca4cb9b8a2328ebc5
Zvonimir <zvonimirplivelic91@gmail.com>
2022-05-10 09:55:25 +0200

    Created layouts for repo and author details. Redesigned list item

--- app/src/main/java/com/zvonimirplivelic/githound/ui/fragments/RepoSearchListFragment.kt
+++ app/src/main/java/com/zvonimirplivelic/githound/ui/fragments/RepoSearchListFragment.kt
@@ -5,17 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.zvonimirplivelic.githound.GitHoundViewModel
 import com.zvonimirplivelic.githound.R
+import com.zvonimirplivelic.githound.model.GitRepoListResponse
 import com.zvonimirplivelic.githound.ui.RepoSearchListAdapter
 
 class RepoSearchListFragment : Fragment() {
     private lateinit var viewModel: GitHoundViewModel
     private lateinit var repoListAdapter: RepoSearchListAdapter
     private lateinit var recyclerView: RecyclerView
+
+    private lateinit var etSearchListQuery: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +28,10 @@ class RepoSearchListFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_search_list, container, false)
 
         viewModel = ViewModelProvider(this)[GitHoundViewModel::class.java]
+
         recyclerView = view.findViewById(R.id.rv_repository_list)
+        etSearchListQuery = view.findViewById(R.id.et_search_list_query)
+
         repoListAdapter = RepoSearchListAdapter()
         recyclerView.apply {
             adapter = repoListAdapter
@@ -34,6 +41,19 @@ class RepoSearchListFragment : Fragment() {
         viewModel.getRepositoryList()
 
         viewModel.repositoryList.observe(viewLifecycleOwner) { repositoryList ->
+
+//            val repoSearch: MutableList<GitRepoListResponse.GitRepoResponseItem> = mutableListOf()
+//            val queryString = etSearchListQuery.text.toString()
+//
+//            repoSearch.forEach { repoItem ->
+//                val repositoryName = repoItem.name
+//                if(repositoryName.contains(queryString)) {
+//                    repoSearch.add(repoItem)
+//                } else {
+//                    repoListAdapter.differ.submitList(repositoryList.data)
+//                }
+//            }
+
             repoListAdapter.differ.submitList(repositoryList.data)
         }
         return view
