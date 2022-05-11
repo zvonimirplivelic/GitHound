2f3360366efe9003e1ce036c6e1fdde016987a2d
Zvonimir <zvonimirplivelic91@gmail.com>
2022-05-11 14:43:39 +0200

    Author screen UI populated

--- app/src/main/java/com/zvonimirplivelic/githound/ui/fragments/AuthorDetailsFragment.kt
+++ app/src/main/java/com/zvonimirplivelic/githound/ui/fragments/AuthorDetailsFragment.kt
@@ -5,14 +5,68 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import com.squareup.picasso.Picasso
+import com.zvonimirplivelic.githound.GitHoundViewModel
 import com.zvonimirplivelic.githound.R
+import com.zvonimirplivelic.githound.model.GitAuthorResponse
+import com.zvonimirplivelic.githound.model.GitRepoListResponse
+import com.zvonimirplivelic.githound.util.Resource
 
 class AuthorDetailsFragment : Fragment() {
+
+    private val args by navArgs<RepositoryDetailsFragmentArgs>()
+    private lateinit var viewModel: GitHoundViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_author_details, container, false)
+        val view = inflater.inflate(R.layout.fragment_author_details, container, false)
+        val selectedRepository: GitRepoListResponse.GitRepoResponseItem = args.currentRepository
+        var authorDetailsData: GitAuthorResponse?
+
+        val ivAuthorAvatar: ImageView = view.findViewById(R.id.iv_avatar_author_details)
+        val tvAuthorName: TextView = view.findViewById(R.id.tv_name_author_details)
+
+
+        viewModel = ViewModelProvider(this)[GitHoundViewModel::class.java]
+        viewModel.getAuthorDetailsResponse(selectedRepository.owner.login)
+
+        viewModel.authorDetails.observe(viewLifecycleOwner) { response ->
+            when (response) {
+                is Resource.Success -> {
+//                    progressBar.isVisible = false
+                    response.data?.let { detailResponse ->
+                        authorDetailsData = detailResponse
+
+                        tvAuthorName.text = authorDetailsData!!.login
+
+                        Picasso.get()
+                            .load(authorDetailsData!!.avatarUrl)
+                            .resize(380, 380)
+                            .into(ivAuthorAvatar)
+                    }
+                }
+
+                is Resource.Error -> {
+//                    progressBar.isVisible = false
+                    response.message?.let { message ->
+                        Toast.makeText(activity, "An error occured: $message", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+
+                is Resource.Loading -> {
+//                    progressBar.isVisible = true
+                }
+            }
+        }
+        return view
     }
 }
