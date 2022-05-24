f0ed0cf97055e459a40ec38e7dd91e53252a6175
Zvonimir <zvonimirplivelic91@gmail.com>
2022-05-24 09:32:49 +0200

    Setting up sorting

--- app/src/main/java/com/zvonimirplivelic/githound/ui/fragments/BottomSheetFilterFragment.kt
+++ app/src/main/java/com/zvonimirplivelic/githound/ui/fragments/BottomSheetFilterFragment.kt
@@ -1,5 +1,6 @@
 package com.zvonimirplivelic.githound.ui.fragments
 
+import android.os.Build.VERSION_CODES.S
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,10 @@ import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.zvonimirplivelic.githound.R
 import com.zvonimirplivelic.githound.model.QueryFilterParameter
+import com.zvonimirplivelic.githound.util.Constants.SORT_FORKS
+import com.zvonimirplivelic.githound.util.Constants.SORT_NONE
+import com.zvonimirplivelic.githound.util.Constants.SORT_OPEN_ISSUES
+import com.zvonimirplivelic.githound.util.Constants.SORT_WATCHERS
 
 class BottomSheetFilterFragment : BottomSheetDialogFragment() {
 
@@ -21,18 +26,35 @@ class BottomSheetFilterFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.bottom_sheet_filter_dialog, container, false)
+        
         val rgSortBy: RadioGroup = view.findViewById(R.id.rg_sort_by)
+        var sortBy: String = SORT_NONE
+
         val rgOrderBy: RadioGroup = view.findViewById(R.id.rg_order_by)
-        val rbSelectedSort: RadioButton =
-            view.findViewById(rgSortBy.checkedRadioButtonId)
-        val rbSelectedOrder: RadioButton =
-            view.findViewById(rgOrderBy.checkedRadioButtonId)
+        var orderBy: String = SORT_NONE
+
         val etResultsPerPage: EditText = view.findViewById(R.id.et_results_per_page)
         val etPageNumber: EditText = view.findViewById(R.id.et_page_number)
 
         val btnCancel: Button = view.findViewById(R.id.btn_cancel)
         val btnApply: Button = view.findViewById(R.id.btn_apply_filters)
 
+        rgSortBy.setOnCheckedChangeListener { _, checkedId ->
+            sortBy = when(checkedId) {
+                R.id.rb_forks -> SORT_FORKS
+                R.id.rb_watchers -> SORT_WATCHERS
+                R.id.rb_open_issues -> SORT_OPEN_ISSUES
+                else -> SORT_NONE
+            }
+        }
+
+        rgOrderBy.setOnCheckedChangeListener { _, checkedId ->
+            orderBy = when(checkedId) {
+                R.id.rb_ascending -> SORT_FORKS
+                R.id.rb_descending -> SORT_WATCHERS
+                else -> SORT_NONE
+            }
+        }
 
 
         btnCancel.setOnClickListener {
@@ -43,8 +65,8 @@ class BottomSheetFilterFragment : BottomSheetDialogFragment() {
             val action =
                 BottomSheetFilterFragmentDirections.actionBottomSheetFilterFragmentToSearchListFragment(
                     QueryFilterParameter(
-                        rbSelectedSort.text.toString(),
-                        rbSelectedOrder.text.toString(),
+                        sortBy,
+                        orderBy,
                         etResultsPerPage.text.toString().toInt(),
                         etPageNumber.text.toString().toInt()
                     )
